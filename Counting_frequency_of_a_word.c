fd70d048801a307a221ba4e8c60eef1c208d533c
princeFoysal2001 <88039167+princeFoysal2001@users.noreply.github.com>
2021-10-24 16:26:14 +0600

    Add files via upload

---
+++ Counting_frequency_of_a_word.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    printf("Enter the longer string: ");
+    char mainStr[128];
+    gets(mainStr);
+    printf("Enter the string to be found: ");
+    char inpWord[20];
+    gets(inpWord);
+    int i = 0,j = 0, freqCount = 0;
+    char tempStr[20];
+    while(mainStr[i] != '\0')
+    {
+        while(!isalpha(mainStr[i]) && mainStr[i] != '\0') {i++; continue;}
+        while(isalpha(mainStr[i]))
+        {
+            tempStr[j] = mainStr[i];
+            i++; j++;
+        }
+        tempStr[j] = '\0';
+        j = 0;
+        printf("%s\n", tempStr);
+        if(mainStr[i] == '\0') break;
+        if(!strcmpi(tempStr, inpWord))
+            freqCount++;
+    }
+    printf("%d", freqCount);
+}
